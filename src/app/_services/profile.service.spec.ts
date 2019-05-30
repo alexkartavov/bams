d695e6624d20f3beef9dd585e7b1f9609ffcaf50
Alex Kartavov <alexanderk@outlook.com>
2019-05-30 12:29:43 -0400

    fixed failing tests

--- src/app/_services/profile.service.spec.ts
+++ src/app/_services/profile.service.spec.ts
@@ -1,12 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ProfileService } from './profile.service';
 
 describe('Service: Profile', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProfileService]
+      providers: [ProfileService],
+      imports: [
+        HttpClientTestingModule
+      ]
     });
   });
 
