369589e591f01d14857a01beb91ea36ea1ce3bb3
Alex Kartavov <alexanderk@outlook.com>
2019-06-26 14:59:20 -0400

    show error messages if not prod

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -5,6 +5,7 @@ import { catchError } from 'rxjs/operators';
 
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -13,20 +14,19 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
-            if ([400, 401, 403].indexOf(err.status) !== -1) {
+            if ([401, 403].indexOf(err.status) !== -1) {
                 // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
                 this.authService.logout();
                 // location.reload(true);
 
                 switch (err.status) {
-                    case 400:
-                        this.alertify.error('Unable to complete request. Verify user credentials.');
-                        break;
                     case 401:
                     case 403:
                         this.alertify.error('User is unauthorized to access.');
                         break;
                 }
+            } else if (!environment.production && err.status >= 400) {
+                this.alertify.error(err.message);
             }
 
             const error = err.message || err.statusText;
