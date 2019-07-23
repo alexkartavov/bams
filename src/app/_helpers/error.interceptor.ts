463227d4842768cb4ea9d744b1af587a7c35b724
Alex Kartavov <alexanderk@outlook.com>
2019-07-23 15:21:08 -0400

    YUM-236 better auth messaging

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -14,15 +14,19 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
-            if ([401, 403].indexOf(err.status) !== -1) {
+            if ([400, 401, 403, 404].indexOf(err.status) !== -1) {
                 // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
                 this.authService.logout();
                 // location.reload(true);
 
                 switch (err.status) {
+                    case 400:
                     case 401:
                     case 403:
                         this.alertify.error('User is unauthorized to access.');
+                        break;
+                    case 404:
+                        this.alertify.error('User is not found.');
                         break;
                 }
             } else if (!environment.production && err.status >= 400 && err.status !== 428) {
